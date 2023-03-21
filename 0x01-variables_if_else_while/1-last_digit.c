46c002b3cee6eb602b620898776aa5866e2b7020
brianMbira <brian.mbira.bm@gmail.com>
2023-03-21 07:21:29 -0700

    push

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,17 +1,16 @@
-	#include <stdlib.h>
-	#include <time.h>
-	/* more headers goes there */
-	#include <stdio.h>
-	/*betty style doc for function main goes there */
-	/**
-	* main - main function
- 	*
-	* Return: always 0
-	*/
-	int main(void)
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/*betty style doc for function main goes there */
+/**
+* main - main function
+*
+* Return: always 0
+*/
+int main(void)
 {
 	int n;
-
 	int m;
 
 	srand(time(0));
