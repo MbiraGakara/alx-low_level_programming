d95cc4cbcbe576729cda42e1d63920410e4b9d8d
brianMbira <brian.mbira.bm@gmail.com>
2023-03-23 14:11:09 -0700

    task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+unsigned long int n = 612852475143;
+unsigned long int i;
+
+while (n % 2 == 0)
+{
+n /= 2;
+}
+
+for (i = 3; i <= sqrt(n); i += 2)
+{
+while (n % i == 0)
+{
+n /= i;
+}
+}
+
+if (n > 2)
+{
+printf("%lu\n", n);
+}
+
+return (0);
+}
+
