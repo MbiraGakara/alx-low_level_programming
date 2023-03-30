36598f66d306500397d2d693e85dc2c4014fc6f7
brianMbira <brian.mbira.bm@gmail.com>
2023-03-30 05:46:12 -0700

    task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using ROT13 encryption.
+ * @str: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+ */
+char *rot13(char *str)
+{
+	char *ptr = str;
+	char *alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (*ptr)
+	{
+		int i = 0;
+
+		while (alpha[i])
+		{
+			if (*ptr == alpha[i])
+			{
+				*ptr = rot13[i];
+				break;
+			}
+
+			i++;
+		}
+
+		ptr++;
+	}
+
+	return (str);
+}
+
