45345d1ae5e34c0c426ce5cadf0fff673190301a
brianMbira <brian.mbira.bm@gmail.com>
2023-04-04 09:20:52 -0700

    task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+/**
+ * set_string - Sets the value of a pointer to a char
+ * @s: Pointer to pointer to char
+ * @to: Pointer to char
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+void set_string(char **s, char *to);
+
+#endif
+
