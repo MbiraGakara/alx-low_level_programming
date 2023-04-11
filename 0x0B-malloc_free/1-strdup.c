fadd908fe0d11b0412afab2b6cdff63edbe39f9a
brianMbira <brian.mbira.bm@gmail.com>
2023-04-11 08:28:16 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+
+char *_strdup(char *str)
+{
+char *dup_str;
+int str_len = 0, i;
+
+if (str == NULL) 
+{
+return NULL;
+}
+
+while (str[str_len] != '\0') 
+{
+str_len++;
+}
+
+dup_str = malloc(sizeof(char) * (str_len + 1));
+
+if (dup_str == NULL) 
+{
+return NULL;
+}
+
+for (i = 0; i < str_len; i++) 
+{
+dup_str[i] = str[i];
+}
+dup_str[str_len] = '\0';
+
+return dup_str;
+}
+
