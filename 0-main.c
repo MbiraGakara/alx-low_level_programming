ad3b11fcc77049b82b46dc3e4c7a80a24b312864
MbiraGakara <brian.mbira.bm@gmail.com>
2023-04-27 12:56:34 -0700

    task 6

--- 0-main.c
+++ 0-main.c
@@ -1,17 +1,20 @@
-#include "main.h"
 #include <stdio.h>
+#include "variadic_functions.h"
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
- */
+* main - check the code
+*
+* Return: Always 0.
+*/
 int main(void)
 {
-char c;
+int sum;
 
-c = 'A';
-printf("%c: %d\n", c, _isupper(c));
-c = 'a';
-printf("%c: %d\n", c, _isupper(c));
+sum = sum_them_all(2, 98, 1024);
+printf("%d\n", sum);
+sum = sum_them_all(4, 98, 1024, 402, -1024);
+printf("%d\n", sum);
 return (0);
+}
+
+
