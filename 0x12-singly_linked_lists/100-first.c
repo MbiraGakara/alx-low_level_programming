5b9667d336893bcc2c96fce0b437a52c2f588eb9
MbiraGakara <brian.mbira.bm@gmail.com>
2023-04-28 12:54:07 -0700

    task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,15 +1,30 @@
-#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-void print_intro() 
+/**
+ * before_main - function that runs before the main function
+ *
+ * This function is marked with the constructor attribute, which means that
+ * it will be automatically called before the main function starts. Its purpose
+ * is to print a message to the standard output file descriptor indicating
+ * that the program is about to start.
+ */
+
+void __attribute__((constructor)) before_main()
 {
-printf("You're beat! and yet, you must allow,\n");
-printf("I bore my house upon my back!\n");
+char *msg = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
+int len = sizeof(msg) - 1;
+char *buffer = malloc(len);
+int i;
+
+for (i = 0; i < len; i++)
+{
+buffer[i] = msg[i];
 }
 
-int main() 
-{
-print_intro();
+write(STDOUT_FILENO, buffer, len);
 
-return (0);
+free(buffer);
+exit(0);
 }
 
